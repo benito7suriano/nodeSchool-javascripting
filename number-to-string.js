592f90d8606724e0aee6ad2185bb4838957a74ad
Beno Suriano <bjsuriano@gmail.com>
2015-08-24 12:54:38 -0600

    my nodeschool javascripting exercises

---
+++ number-to-string.js
@@ -0,0 +1,6 @@
+var n = 128;
+
+var N = n.toString();
+
+console.log(N);
+
---
+++ revising-strings.js
@@ -0,0 +1,6 @@
+var pizza = 'pizza is alright';
+
+var newPizza = pizza.replace('alright','wonderful');
+
+console.log(newPizza);
+
